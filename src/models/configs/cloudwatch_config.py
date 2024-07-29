8eca9ff48d62fbf5fecef088dd66d403b5174110
tf0rs <trevorcforsyth@gmail.com>
2024-07-29 14:53:45 +0100

    feat: bytewax source for cloudwatch logs

--- src/models/configs/cloudwatch_config.py
+++ src/models/configs/cloudwatch_config.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
-from pydantic import StrictStr, BaseModel
+from pydantic import StrictStr, BaseModel, PositiveInt
+
+from models.configs.indexer_config import get_indexer_config
 
 
 class CloudwatchConfig(BaseModel):
@@ -8,4 +10,17 @@ class CloudwatchConfig(BaseModel):
     aws_access_key_id: StrictStr
     aws_secret_access_key: StrictStr
     log_group: StrictStr
+    start_time: PositiveInt
     filter_pattern: Optional[StrictStr] = None
+
+
+def get_cloudwatch_config() -> CloudwatchConfig:
+    config = get_indexer_config()
+    return CloudwatchConfig(
+        region_name=config.CLOUDWATCH_REGION,
+        aws_access_key_id=config.CLOUDWATCH_AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=config.CLOUDWATCH_AWS_SECRET_ACCESS_KEY,
+        log_group=config.CLOUDWATCH_LOG_GROUP,
+        start_time=config.CLOUDWATCH_START_TIME,
+        filter_pattern=config.CLOUDWATCH_FILTER_PATTERN,
+    )
