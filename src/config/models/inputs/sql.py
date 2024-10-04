616e19038e6d17415aeaabf139a32b88ac581544
samtin0x <40127309+samtin0x@users.noreply.github.com>
2024-10-04 10:39:19 +0100

    chore: add SQL validation logic to ensure required parameters and fields.

--- src/config/models/inputs/sql.py
+++ src/config/models/inputs/sql.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, StrictStr
+from pydantic import BaseModel, StrictStr, field_validator
 
 from src.clients.postgres import PostgresConfig
 
@@ -6,3 +6,24 @@ from src.clients.postgres import PostgresConfig
 class SqlConfig(BaseModel):
     sql: StrictStr
     metrics_config: PostgresConfig
+
+    @field_validator("sql")
+    def validate_sql_parameters(cls, v):
+        required_params = [":start_time", ":end_time"]
+        for param in required_params:
+            if param not in v:
+                raise ValueError(
+                    f"SQL query must contain the parameter {param} in the query to be replaced"
+                )
+        return v
+
+    @field_validator("sql")
+    def validate_sql_fields(cls, v):
+        required_fields = ["as timestamp", "as customer_id"]
+        lower_sql = v.lower()
+        for field in required_fields:
+            if field not in lower_sql:
+                raise ValueError(
+                    f"SQL query must select the field '{field}'. Field needs to be aliased as '{field}'"
+                )
+        return v
